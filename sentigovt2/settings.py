1524d7ed8f08a0ebb3b663937548cc3e5b7791f5
Alvin Andrian Rizki <80727692+alvinrrizky@users.noreply.github.com>
2023-04-30 10:39:36 +0700

    Add tailwind & update static media

--- sentigovt2/settings.py
+++ sentigovt2/settings.py
@@ -128,10 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+TAILWIND_CSS = 'static/css/tailwind.css'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
