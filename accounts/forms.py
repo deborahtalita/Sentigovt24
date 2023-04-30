8e78b976b8376b69a6992537c6bd9c62d8a0644b
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2023-04-30 16:25:13 +0700

    merge

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ class SignUpForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'avatar')
+        fields = ('username', 'email', 'first_name',  'password1', 'password2')
     
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
