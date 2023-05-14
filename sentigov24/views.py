506f0a9d0377e36161fcf0643343cc24cc96f930
Alvin Andrian Rizki <80727692+alvinrrizky@users.noreply.github.com>
2023-05-14 20:20:55 +0700

    Update All Project

---
+++ sentigov24/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def dashboard(request):
+    context = {'active_page': 'dashboard'}
+    return render(request, 'dashboard.html', context)
+
+def manualSearch(request):
+    context = {'active_page': 'manual search'}
+    return render(request, 'manualSearch.html', context)
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def history(request):
+    context = {'active_page': 'history'}
+    return render(request, 'history.html', context)
+
+def userManagement(request):
+    context = {'active_page': 'user management'}
+    return render(request, 'userManagement.html', context)
+
+def bacapresManagement(request):
+    context = {'active_page': 'bacapres management'}
+    return render(request, 'bacapresManagement.html', context)
+
+def createBacapres(request):
+    context = {'active_page': 'bacapres management'}
+    return render(request, 'createBacapres.html', context)
--- sentiment/forms/bacapres_form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from sentiment.models import Bacapres
-
-class BacapresForm(forms.ModelForm):
-
-    class Meta:
-        model = Bacapres
-        fields = ('name', 'keyword', 'desc')
