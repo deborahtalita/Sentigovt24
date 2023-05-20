e60ea06dc8a03b951591744f3ea1d1c45cbe99b2
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2023-05-21 05:39:17 +0700

    update on dashboard

--- sentigovt2/views.py
+++ sentigovt2/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from sentiment.models import Tweet, Bacapres
 import uuid
+from django.db.models import Count
 from datetime import datetime, timedelta
 from django.utils.timezone import make_aware
+from django.core.paginator import Paginator
 import pytz 
 
 # Create your views here.
@@ -43,16 +45,49 @@ def dashboard(request):
     seven_days_ago = today - timedelta(days=6)
     tweet = Tweet.objects.filter(created_at__gte=seven_days_ago)
 
-    i = 1
-    for t in tweet:
-        i=i+1
-    print(i)
-    print(counter)
+    # total tweet per day per classification
+    counter = tweet.extra({
+        'created_at': "date(created_at)"
+    }).values('created_at').distinct().annotate(Count('created_at'))
+
+    dates = []
+    dates_counter = []
+
+    for i in counter:
+        dates.append(i['created_at'])
+        dates_counter.append(i['created_at__count'])
+    
+    context['dates'] = dates
+
+
+    tokoh_tweets = Tweet.objects.filter(bacapres=11)
+
+    # total tweet
+    total = tokoh_tweets.count()
+    context['total_tweet'] = total
+
+    neg_sentiment = tokoh_tweets.filter(sentiment='negative').count()
+    pos_sentiment = tokoh_tweets.filter(sentiment='positive').count()
+    neu_sentiment = tokoh_tweets.filter(sentiment='neutral').count()
+    
+    # total tweet per classification
+    context['sentiment'] = {'negative':neg_sentiment,
+                            'positive':pos_sentiment,
+                            'neutral':neu_sentiment}
+    context['pos_sentiment'] = pos_sentiment
+    context['neu_sentiment'] = neu_sentiment
 
     # ranking bacapres
     bacapres = Bacapres.objects.all()
     context['bacapres'] = bacapres
 
+    #tweet list with pagination
+    paginator = Paginator(tokoh_tweets, 10)  # 10 items per page
+    page_number = request.GET.get('page')  # Get the current page number from the request
+    page_obj = paginator.get_page(page_number)
+    context['page_obj'] = page_obj
+
+    context['title'] = 'Dashboard'
     context['active_page'] = 'dashboard'
     return render(request, 'dashboard.html', context)
 
